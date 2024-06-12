df6ac99db50334704b0678a6afba77bf6b65e501
muknah <nomsaraa@gmail.com>
2024-06-12 05:24:41 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94705193004', 'me', true],
+  ['94705193004', 'ZANTA_AKASH', true], 
   [''] 
 ] //Number of owners
 
